209521f37568583b277ae77e72fe6ab1484a637d
Hajar Ayaddouz <hajarayaddouz29@gmail.com>
2024-05-22 16:14:01 +0200

    Added some design with bootstrap classes

--- recap.php
+++ recap.php
@@ -7,6 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Récapitulatif des produits</title>
 </head>
 <body>
@@ -41,9 +42,9 @@
                             "<td>".$index."</td>",
                             "<td>".$product['name']."</td>",
                             "<td>".number_format($product['price'], 2, ",", "&nbsp;")."&nbsp;€.</td>",
-                            "<td>"."<a class='link-offset-2 link-underline link-underline-opacity-0' href=traitement.php?action=down-qtt&id=$index>-</a>".$product['qtt']."<a class='link-offset-2 link-underline link-underline-opacity-0' href=traitement.php?action=up-qtt&id=$index> + </a>"."</td>",
+                            "<td>"."<button class='btn btn-outline-danger' id ='down' type='submit' name='down' > <a class='link-offset-2 link-underline link-underline-opacity-0' href=traitement.php?action=down-qtt&id=$index>-</a> </button>".$product['qtt']."<button class='btn btn-outline-success' id='up' type='submit' name='up'> <a class='link-offset-2 link-underline link-underline-opacity-0' href=traitement.php?action=up-qtt&id=$index> + </a></button>"."</td>",
                             "<td>".number_format($product['total'], 2, ",", "&nbsp;")."&nbsp;€.</td>",
-                            "<td> <a class='link-offset-2 link-underline link-underline-opacity-0' href=traitement.php?action=delete&id=$index> X </a> </td>",
+                            "<td> <a class='link-offset-2 link-underline link-underline-opacity-0' href=traitement.php?action=delete&id=$index> <i class='fa-solid fa-trash'></i> </a> </td>",
                         "</tr>";
                     $totalGeneral+= $product['total'];
                     $nbProduit += $product['qtt'];
@@ -60,7 +61,7 @@
                     "</tr>",
               "</tbody>",
             "</table>";
-            echo "<button class='btn btn-danger' id='deleteAll' type='submit' name='clear'> <a class='link-offset-2 link-underline link-underline-opacity-0' href=traitement.php?action=clear> Supprimer tout le panier </a> </button>";
+            echo "<button class='btn btn-danger' id='deleteAll' type='submit' name='clear'> <a class='text-reset' href=traitement.php?action=clear> Supprimer tout le panier </a> </button>";
         }
         
         if (isset($_SESSION["message"]))
